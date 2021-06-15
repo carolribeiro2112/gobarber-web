f3f8636e33b2612c65579e15bd9e69f1d7e05280
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2021-06-15 14:59:41 -0300

    estrutura de toast

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,25 @@ import SignIn from './Pages/SignIn';
 // import {BrowserRouter} from 'react-router-dom';
 // import Routes from './routes';
 
-import SignUp from './Pages/SignUp';
+// import SignUp from './Pages/SignUp';
 
 import GlobalStyle from './styles/global';
 
+import ToastContainer from './components/ToastContainer';
+import { AuthProvider } from './hooks/AuthContext';
+
+
 function App() {
+  
   return (
     <>
-      <SignUp/>
-      {/* <BrowserRouter>
-        <Routes/>
-      </BrowserRouter> */}
+      <AuthProvider>
+        <SignIn/>
+        {/* <BrowserRouter>
+          <Routes/>
+        </BrowserRouter> */}
+      </AuthProvider>
+      <ToastContainer/>
       <GlobalStyle/>
     </>
   );
--- src/Pages/SignIn/index.tsx
+++ src/Pages/SignIn/index.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
-import {FiLogIn, FiMail, FiLock} from 'react-icons/fi'
+import React, { useRef, useCallback } from 'react';
+import {FiLogIn, FiMail, FiLock} from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+
+import { useAuth } from '../../hooks/AuthContext';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -8,26 +14,63 @@ import Button from '../../components/Button';
 
 import {Container, Content, Background} from './styles';
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
 const SignIn = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const { signIn } = useAuth();
+
+  const handleSubmit = useCallback(
+    async(data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          email: Yup.string().required('E-mail obrigatório').email('Digite um E-mail váilido'),
+          password: Yup.string().required('Senha obrigatória'),
+        });
+        await schema.validate(data,{
+          abortEarly: false,
+        });
+
+        signIn({
+          email: data.email,
+          password: data.password,
+        });
+      } catch(err) {
+        if(err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+        }
+        // disparar toast 
+        
+      }
+  }, [signIn]);
+
   return(
     <Container>
       <Content>
         <img src={logoImg} alt="GoBarber"/>
 
-        <form>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça o seu Logon</h1>
           <Input name="email" icon={FiMail} type="text" placeholder="E-mail"/>
           <Input name="password" icon={FiLock} type="password" placeholder="Senha"/>
           <Button type="submit">Entrar</Button>
 
           <a href="teste">Esqueci minha senha</a>
-        </form>
+        </Form>
 
         <a href="create"><FiLogIn size={20}/>Criar conta</a>
       </Content>
       <Background/>
     </Container>
-  )
+  );
 }
 
 export default SignIn;
