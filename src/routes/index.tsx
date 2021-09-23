955920f7bb415d3ac62f6ea29aa3703e0d260d21
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2021-09-23 18:12:52 -0300

    password recovery

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,6 +7,7 @@ import SignIn from '../Pages/SignIn';
 import SignUp from '../Pages/SignUp';
 import Dashboard from '../Pages/Dashboard';
 import ForgotPassword from '../Pages/ForgotPassword';
+import ResetPassword from '../Pages/ResetPassword';
 
 const Routes: React.FC = () => {
   return(
@@ -14,6 +15,8 @@ const Routes: React.FC = () => {
       <Route path='/' exact component={SignIn}/>
       <Route path='/signup' component={SignUp}/>
       <Route path='/forgot-password' component={ForgotPassword}/>
+      <Route path='/reset-password' component={ResetPassword}/>
+      
 
       <Route path='/dashboard' isPrivate component={Dashboard}/>
     </Switch>
