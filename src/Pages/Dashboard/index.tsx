5d29debab0a31aa15d621e9cd1ad419d32040dd3
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2021-09-27 10:27:47 -0300

    month availability

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import DayPicker, {DayModifiers} from "react-day-picker";
 import 'react-day-picker/lib/style.css';
 
@@ -19,18 +19,54 @@ import { FiClock, FiPower } from "react-icons/fi";
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from "../../hooks/AuthContext";
+import api from "../../services/api";
+
+interface IMonthAvailabilityItem {
+  day: number;
+  available: boolean;
+}
 
 
 const Dashboard = () => {
+  const { signOut, user } = useAuth();
+
   const [selectedDate, setSelectedDate] = useState(new Date());
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+  const [monthAvailability, setMonthAvailability] = useState<IMonthAvailabilityItem[]>([]);
 
   const handleDateChange = useCallback((day: Date, modifiers: DayModifiers)=>{
     if(modifiers.available) {
       setSelectedDate(day)
     }
-  }, [])
+  }, []);
 
-  const { signOut, user } = useAuth();
+  const handleMonthChange = useCallback((month:Date)=>{
+    setCurrentMonth(month)
+  },[])
+
+  useEffect(()=>{
+    api.get(`/providers/${user.id}/month-availability`, {
+      params: {
+        year: currentMonth.getFullYear(),
+        month: currentMonth.getMonth() + 1,
+      }
+    }).then(response => {
+      setMonthAvailability(response.data)
+    })
+  },[currentMonth, user.id])
+
+  const disabledDays = useMemo(()=>{
+    const dates = monthAvailability
+      .filter(monthDay => monthDay.available === false)
+      .map(monthDay => {
+        const year = currentMonth.getFullYear();
+        const month = currentMonth.getMonth();
+
+        return new Date(year, month, monthDay.day);
+      })
+      return dates;
+
+  }, [currentMonth, monthAvailability])
   
   return(
     <Container>
@@ -125,11 +161,13 @@ const Dashboard = () => {
             weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
             fromMonth={new Date()}
             disabledDays={[
-              {daysOfWeek:[0,6]}
+              {daysOfWeek:[0,6]},
+              ...disabledDays
             ]}
             modifiers={{
               available: { daysOfWeek: [1, 2, 3, 4, 5]}
             }}
+            onMonthChange={handleMonthChange}
             selectedDays={selectedDate}
             onDayClick={handleDateChange}
             months={[
